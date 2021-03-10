48a6d801f94d977219b169418db08dcd783ad71c
Abdul Aziz <dev.abdaziz@outlook.com>
2021-03-10 16:25:40 +0700

    jos

---
+++ 25-JAVA/Belajar Java Dasar/src/data/Customer.java
@@ -0,0 +1,22 @@
+package data;
+
+public class Customer {
+    public String Name;
+    public Boolean value;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public Boolean getValue() {
+        return value;
+    }
+
+    public void setValue(Boolean value) {
+        this.value = value;
+    }
+}
