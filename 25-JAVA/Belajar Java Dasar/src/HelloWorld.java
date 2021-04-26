62a944501ed761324b833b11f6a629bfce93ce2e
Abdul Aziz <dev.abdaziz@outlook.com>
2021-04-26 16:13:32 +0700

    jos

--- 25-JAVA/Belajar Java Dasar/src/HelloWorld.java
+++ 25-JAVA/Belajar Java Dasar/src/HelloWorld.java
@@ -2,12 +2,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
 
-        final var nama = "abdaziz";
-        System.out.println(nama);
 
-        String title = "jos";
-        int x = 0;
-        Integer y = 100;
 
     }
 }
